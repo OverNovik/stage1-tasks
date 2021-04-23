6924fe9cf8233a929378cac97e64173fae94b7fe
Aliaxander Novikov <nerevarin11@gmail.com>
2021-04-23 18:39:43 +0300

    feat: add css filters, reset btn, fullscreen btn

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -2,18 +2,39 @@
 const inputs = document.querySelectorAll('.filters input');
 const outputs = document.querySelectorAll('.filters output');
 const fullscreenBtn = document.querySelector('.fullscreen');
+const resetBtn = document.querySelector('.btn-reset')
 
 
-function handleUpdate() {
-    const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    console.log(suffix)
+function handleUpdate(e) {
+  const suffix = e.target.dataset.sizing || '';
+
+	setFilter(e.target.name, e.target.value + suffix);
+	e.target.nextElementSibling.innerHTML = e.target.value;
 }
 
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+resetBtn.addEventListener('click', () => {
+	inputs.forEach(input => {
+		let resetValue = 0;
 
+		if (input.name === 'saturate') {
+			resetValue = 100;
+		}
+		const suffix = input.dataset.sizing || '';
+
+		setFilter(input.name, resetValue + suffix)
+		input.value = resetValue
+		input.nextElementSibling.innerHTML = resetValue;
+	})
+	
+})
+
+function setFilter(name, value) {
+	document.documentElement.style.setProperty(`--${name}`, value);
+}
+
+inputs.forEach(input => input.addEventListener('input', handleUpdate));
+ 
 fullscreenBtn.addEventListener('click', () => {
-    if(document.documentElement.requestFullscreen) document.documentElement.requestFullscreen();
-    if(document.fullscreenEnabled) document.exitFullscreen();
+  if(document.documentElement.requestFullscreen) document.documentElement.requestFullscreen();
+  if(document.fullscreenEnabled) document.exitFullscreen();
 });
